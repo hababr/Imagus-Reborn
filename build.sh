5c2b08c0ddcfef27bb2e4dcf3799d50410c4a369
Ali Homafar <home.isfar@gmail.com>
2026-05-14 00:43:54 -0400

    pushd and popd in the build script

--- build.sh
+++ build.sh
@@ -13,17 +13,17 @@ rm -rf ./build/* || die
 echo -e "\nCopying '$src' to 'chrome'..."
 cp -r ./$src/. ./build/chrome/ || die
 echo "Zipping the Chrome build..."
-cd ./build/chrome || die
+pushd ./build/chrome || die
 rm -f manifest_firefox.json || die
 zip -9 -q -r ../ImagusReborn_Chrome_v`sed -n -E 's/^.*"version":\s*"(\S*)".*/\1/p' manifest.json`.zip *
+popd
 
-cd ../..
 echo -e "\nCopying '$src' to 'firefox'..."
 cp -r ./$src/. ./build/firefox/ || die
 echo "Zipping the Firefox build..."
-cd ./build/firefox || die
+pushd ./build/firefox || die
 mv -f manifest_firefox.json manifest.json
 zip -9 -q -r ../ImagusReborn_Firefox_v`sed -n -E 's/^.*"version":\s*"(\S*)".*/\1/p' manifest.json`.zip *
+popd
 
 echo -e "\nDone!"
-waitkey
